47c0ec4f555d0bd004a9f2984161a3da9662c2b9
Jesse van Elteren <jvanelteren@gmail.com>
2021-04-02 21:32:52 +0200

    change future upwards

--- aocutils/__init__.py
+++ aocutils/__init__.py
@@ -1,3 +1,5 @@
 __version__ = "0.0.1"
 # this line will enable you to call desired function without prepending aoc_utils.aoc all the time
 from aocutils.aoc import *
+from aocutils.aocnorvig import *
+from aocutils.aoccfg import *
